fb340cce909248d0e53df15233f004188fc16ee1
Marcos Veron <mveron@cdainfo.com>
2020-02-18 13:24:50 -0300

    mejoras en index-mobil

---
+++ video.js
@@ -0,0 +1,13 @@
+var vid = document.getElementById("myVideo"); 
+
+function myFunction() {
+    vid.controls = true;
+}
+
+function playVid() { 
+  vid.play(); 
+} 
+
+function pauseVid() { 
+  vid.pause(); 
+} 
